0e3a446ddc25069b6198dd37d85f6cf3ac965cba
Jacob Schwartz <jacob@jtschwartz.com>
2021-03-25 17:35:07 -0400

    Replacement functions

--- src/functions/strings.ts
+++ src/functions/strings.ts
@@ -1,6 +1,15 @@
+const replaceAll = (str: string, original: string, replacement: string): string => {
+	return str.split(original).join(replacement)
+}
+
 export const stringFunctions = {
 	capitalize: (alterVal: string): string => {
 		return alterVal && alterVal.charAt(0).toUpperCase() + alterVal.slice(1)
+	},
+	replaceAll: replaceAll,
+	replacementMap: (str: string, map: {[key: string]: string}): string => {
+		for (let key of Object.keys(map)) replaceAll(str, key, map[key])
+		return str
 	},
 	sentenceCase: (alterVal: string): string => {
 		return alterVal && alterVal.charAt(0).toUpperCase() + alterVal.slice(1).toLowerCase()
